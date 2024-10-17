5b08f2cfa3d166ceac35de109500deafdda4075e
Pedro Wrublak <pedrovili.w@gmail.com>
2024-10-16 23:49:09 -0300

    atualização da luzinha

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const light = document.querySelector('.light');
 
 document.addEventListener('mousemove', (event) => {
-    light.style.left = event.clientX + 'px';
-    light.style.top = event.clientY + 'px';
+    light.style.left = event.layerX + 'px';
+    light.style.top = event.layerY + 'px';
 });
